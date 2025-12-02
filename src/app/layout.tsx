87227dd776ceebcc37258058e22e218262fcc0ea
Vasil Vasilev <vasilev@palantir.com>
2025-12-02 14:24:36 +0200

    Revert "fonts"
    This reverts commit 410621589d12778d702f7826e5b586f2c29c4a5b.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,16 @@
 import type { Metadata } from "next";
-import { GeistSans } from "geist/font/sans";
-import { GeistMono } from "geist/font/mono";
+import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
-const geistSans = GeistSans;
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
 
-const geistMono = GeistMono;
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
 
 export const metadata: Metadata = {
   title: "Hello Apollo",
