447cfc6e0d88ae78c3136269564aeac1ea279c4c
Vasil Vasilev <vasilev@palantir.com>
2026-05-28 11:51:18 +0300

    Optional secrets

--- src/app/api/hello/route.ts
+++ src/app/api/hello/route.ts
@@ -3,9 +3,9 @@ import { NextResponse } from 'next/server';
 export async function GET() {
   return NextResponse.json({ 
     env: {
-      NEXT_PUBLIC_VALUE_FROM_SECRET: process.env.NEXT_PUBLIC_VALUE_FROM_SECRET || "",
-      NEXT_PUBLIC_VALUE_FROM_MODULE_VARIABLE: process.env.NEXT_PUBLIC_VALUE_FROM_MODULE_VARIABLE || "",
-      NEXT_PUBLIC_VALUE_FROM_ENVIRONMENT_CONFIG: process.env.NEXT_PUBLIC_VALUE_FROM_ENVIRONMENT_CONFIG || ""
+      NEXT_PUBLIC_VALUE_FROM_SECRET: process.env.NEXT_PUBLIC_VALUE_FROM_SECRET || "(secret not configured)",
+      NEXT_PUBLIC_VALUE_FROM_MODULE_VARIABLE: process.env.NEXT_PUBLIC_VALUE_FROM_MODULE_VARIABLE || "(module variable not set)",
+      NEXT_PUBLIC_VALUE_FROM_ENVIRONMENT_CONFIG: process.env.NEXT_PUBLIC_VALUE_FROM_ENVIRONMENT_CONFIG || "(environment config not set)"
     },
     timestamp: new Date().toISOString()
   });
